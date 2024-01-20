5605c7b06c8ea0f1fd2957f4ad2fa67823c317bd
Anant Shanbhag <anant.s888@gmail.com>
2024-01-20 13:33:41 +0530

    1-1-Add Components - Cake

--- 1-react-redux-demo/src/App.js
+++ 1-react-redux-demo/src/App.js
@@ -1,5 +1,7 @@
+import CakeContainer from "./components/CakeContainer";
+
 function App() {
-  return <div>Hello World</div>;
+  return <CakeContainer />;
 }
 
 export default App;
---
+++ 1-react-redux-demo/src/components/CakeContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CakeContainer = () => (
+  <>
+    <h2>Number of Cakes</h2>
+    <button>Buy Cake</button>
+  </>
+);
+
+export default CakeContainer;
