0d2285cc0289ab1dc08040ef285487ba4c70c9e4
Anant Shanbhag <anant.s888@gmail.com>
2024-01-22 11:35:54 +0530

    2-add store, actions and reducer

--- 1-react-redux-demo/src/App.js
+++ 1-react-redux-demo/src/App.js
@@ -1,7 +1,14 @@
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
 
 function App() {
-  return <CakeContainer />;
+  return (
+    <Provider store={store}>
+      <CakeContainer />
+    </Provider>
+  );
 }
 
 export default App;
