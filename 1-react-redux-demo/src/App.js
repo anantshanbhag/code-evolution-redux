7a90cf5f5d6220a08e0cb40b6a706e8d17a3df20
Anant Shanbhag <anant.s888@gmail.com>
2024-01-22 14:17:49 +0530

    3- Alternate: use Redux Hooks

--- 1-react-redux-demo/src/App.js
+++ 1-react-redux-demo/src/App.js
@@ -2,11 +2,13 @@ import { Provider } from "react-redux";
 
 import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <CakeContainer />
+      <HooksCakeContainer />
     </Provider>
   );
 }
